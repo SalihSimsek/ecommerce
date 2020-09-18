b3cef0cf3b84ea1b4929c3eb8a1c71088394aade
salihsimseek@gmail.com <salihsimseek@gmail.com>
2020-09-18 14:16:28 +0300

    third commit,js and cart

--- store/models.py
+++ store/models.py
@@ -11,7 +11,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=7,decimal_places=2)
     #Ürün kargolanabilecek bir ürün mü yoksa dijital servis edilebilecek bir ürün mü?
     digital = models.BooleanField(default=False,null=True,blank=True)
     image = models.ImageField(null=True,blank=True)
@@ -45,6 +45,15 @@ class Order(models.Model):
         orderitems = self.orderitem_set.all()
         total = sum([item.quantity for item in orderitems])
         return total
+
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping=True
+        return shipping
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
